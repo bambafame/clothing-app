d891cbab9f6f13b158cfe32eeb93d66f512a1391
bambafame <105991062+bambafame@users.noreply.github.com>
2026-06-30 03:18:47 -0500

    Navigation bar added

---
+++ src/routes/navigation/navigation.jsx
@@ -0,0 +1,27 @@
+import { Link, Outlet } from "react-router-dom";
+import { Fragment } from "react";
+
+import { ReactComponent as AppLogo } from "../../assets/crown.svg";
+
+import "./navigation.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <AppLogo className="logo" />
+        </Link>
+
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
